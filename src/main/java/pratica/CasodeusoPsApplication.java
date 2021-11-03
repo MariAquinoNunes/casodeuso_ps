9bff03db2fc7b1ba0f33b26fe50fdac339db8090
MariAquinoNunes <mariaquinonunes@gmail.com>
2021-11-02 22:07:45 -0300

    Ṕroduto e associação muitos para muitos

--- src/main/java/pratica/CasodeusoPsApplication.java
+++ src/main/java/pratica/CasodeusoPsApplication.java
@@ -8,12 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import pratica.domain.Categoria;
+import pratica.domain.Produto;
 import pratica.repositories.CategoriaRepository;
+import pratica.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CasodeusoPsApplication implements CommandLineRunner{ 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CasodeusoPsApplication.class, args);
 	}
@@ -23,7 +28,18 @@ public class CasodeusoPsApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Computador", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-			
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));	
 	}
 }
