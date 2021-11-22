167d07709a6c59dd288b56e9dbe08b433ec104eb
MariAquinoNunes <mariaquinonunes@gmail.com>
2021-11-22 10:48:02 -0300

    Estado, Cidade e TipoCliente

--- src/main/java/pratica/CasodeusoPsApplication.java
+++ src/main/java/pratica/CasodeusoPsApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import pratica.domain.Categoria;
+import pratica.domain.Cidade;
+import pratica.domain.Estado;
 import pratica.domain.Produto;
 import pratica.repositories.CategoriaRepository;
+import pratica.repositories.CidadeRepository;
+import pratica.repositories.EstadoRepository;
 import pratica.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -18,7 +22,10 @@ public class CasodeusoPsApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CasodeusoPsApplication.class, args);
 	}
@@ -41,5 +48,18 @@ public class CasodeusoPsApplication implements CommandLineRunner{
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));	
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2)); //primeiro salvamos o estado, pq não existe cidade sem estado
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 	}
 }
--- src/main/java/pratica/domain/Cidade.java
+++ src/main/java/pratica/domain/Cidade.java
@@ -1,8 +1,6 @@
 package pratica.domain;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
@@ -10,36 +8,27 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import javax.persistence.ManyToOne;
 
 @Entity
-public class Produto implements Serializable{
+public class Cidade implements Serializable{
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	private Double preco;
+	@ManyToOne
+	@JoinColumn(name="estado_id")
+	private Estado estado;
 	
-	@ManyToMany
-	@JoinTable(name = "PRODUTO_CATEGORIA",
-			joinColumns = @JoinColumn(name = "produto_id"),
-			inverseJoinColumns = @JoinColumn(name = "categoria_id")
-	)
-	@JsonBackReference
-	private List<Categoria> categorias = new ArrayList<>();
-	
-	public Produto() {	
+	public Cidade () {
 	}
 
-	public Produto(Integer id, String nome, Double preco) {
+	public Cidade(Integer id, String nome, Estado estado) {
 		super();
 		this.id = id;
 		this.nome = nome;
-		this.preco = preco;
+		this.estado = estado;
 	}
 
 	public Integer getId() {
@@ -58,20 +47,12 @@ public class Produto implements Serializable{
 		this.nome = nome;
 	}
 
-	public Double getPreco() {
-		return preco;
+	public Estado getEstado() {
+		return estado;
 	}
 
-	public void setPreco(Double preco) {
-		this.preco = preco;
-	}
-
-	public List<Categoria> getCategorias() {
-		return categorias;
-	}
-
-	public void setCategorias(List<Categoria> categorias) {
-		this.categorias = categorias;
+	public void setEstado(Estado estado) {
+		this.estado = estado;
 	}
 
 	@Override
@@ -87,8 +68,10 @@ public class Produto implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Produto other = (Produto) obj;
+		Cidade other = (Cidade) obj;
 		return Objects.equals(id, other.id);
 	}
 	
+	
 }
+
