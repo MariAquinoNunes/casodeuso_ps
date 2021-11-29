0298ef4ff7aea188112385e6373ac7e6b4fccdf1
MariAquinoNunes <mariaquinonunes@gmail.com>
2021-11-29 18:52:12 -0300

    ItemPedido e ItemPedidoPk

--- src/main/java/pratica/CasodeusoPsApplication.java
+++ src/main/java/pratica/CasodeusoPsApplication.java
@@ -13,6 +13,7 @@ import pratica.domain.Cidade;
 import pratica.domain.Cliente;
 import pratica.domain.Endereco;
 import pratica.domain.Estado;
+import pratica.domain.ItemPedido;
 import pratica.domain.Pagamento;
 import pratica.domain.PagamentoComBoleto;
 import pratica.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import pratica.repositories.CidadeRepository;
 import pratica.repositories.ClienteRepository;
 import pratica.repositories.EnderecoRepository;
 import pratica.repositories.EstadoRepository;
+import pratica.repositories.ItemPedidoRepository;
 import pratica.repositories.PagamentoRepository;
 import pratica.repositories.PedidoRepository;
 import pratica.repositories.ProdutoRepository;
@@ -47,6 +49,8 @@ public class CasodeusoPsApplication implements CommandLineRunner{
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CasodeusoPsApplication.class, args);
@@ -111,5 +115,19 @@ public class CasodeusoPsApplication implements CommandLineRunner{
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 0.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
+		
 	}
 }
